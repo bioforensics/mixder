7fdda0afafc3ae42dc12c6357e59ed39ec162e43
rnmitchell <rebecca.mitchell5987@gmail.com>
2025-03-21 10:47:43 -0400

    3D PCA plots

--- R/ancestry_prediction.R
+++ R/ancestry_prediction.R
@@ -18,14 +18,15 @@
 #'
 #' @import kgp
 #' @import ggplot2
+#' @import plotly
 #'
 #' @return NA
 #' @export
 #'
 ancestry_prediction = function(report, path, id, analysis_type, contrib_status) {
   ## using R library to obtain ancestry info for 1000G samples
-  ancestry=kgp::kgp3[,c("id", "reg")]
-  ancestry_filt = subset(ancestry, reg != "SAS")
+  #ancestry=kgp::kgp3[,c("id", "reg")]
+  #ancestry_filt = subset(ancestry, reg != "SAS")
   geno=mixder::ancestry_1000G_all
   geno_filt=geno[,c(7:10030)]
   snps = data.frame("snp_id"=colnames(geno_filt))
@@ -56,32 +57,55 @@ ancestry_prediction = function(report, path, id, analysis_type, contrib_status) 
   PCs = data.frame(pcaRed$x)
 
   ## add unknown to ancestry and genotype IDs
-  ancestry_unk = ancestry %>%
+  #ancestry_unk = ancestry %>%
     add_row(id = "Unk", reg = "Unk")
   geno_unk = geno %>%
     add_row(IID="Unk")
 
+  ancestry = read.table("/Users/rebecca.mitchell/Desktop/ancestry_colors.txt", header=T, sep="\t") %>%
+    add_row(id = "Unk", reg = "Unk", population = "Unk", color="red", superpop_color="red")
   ## merge genotypes with ancestry info; need to preserve order to match to PCA data
-  geno_ancestry=merge(geno_unk, ancestry_unk, by.x="IID", by.y="id")
+  geno_ancestry=merge(geno_unk, ancestry, by.x="IID", by.y="id")
 
   ## add ancestry info to PC data
-  PCs_anc = cbind(ancestry=geno_ancestry[,c(10031)], data.frame(PCs))
+  PCs_anc = cbind(ancestry=geno_ancestry[,c(10031:10032)], data.frame(PCs[,c(1:10)]))
 
-  colScale <- scale_color_manual(name = "Superpopulation",
-                                 values = c("AFR" = "lightblue3",
-                                            "EAS" = "chartreuse2",
-                                            "EUR" = "pink2",
-                                            "AMR" = "gold3",
-                                            "Unk" = "firebrick3"),
-                                 labels = levels(as.factor(PCs_anc$ancestry)))
+  #colScale <- scale_color_manual(name = "Superpopulation",
+  #                               values = c("AFR" = "lightblue3",
+  #                                          "EAS" = "chartreuse2",
+  #                                          "EUR" = "pink2",
+  #                                          "AMR" = "gold3",
+  #                                          "Unk" = "firebrick3"),
+  #                               labels = levels(as.factor(PCs_anc$ancestry)))
 
   ## PCA plot
-  png(glue("{path}/{id}_{contrib_status}_{analysis_type}_PCA_plot.png"))
-  show(ggplot(PCs_anc, aes(x=PC1,y=PC2))+
-    geom_point(aes(color=ancestry))+
-    colScale+
-    guides(color=guide_legend(title="Superpopulation"))+
-    ggtitle(glue("{id} {contrib_status} {analysis_type};\n{ncol(betaRedNAOmit)} ancestry SNPs"))+
-    theme(plot.title = element_text(hjust = 0.5)))
-  dev.off()
+  #png(glue("{path}/{id}_{contrib_status}_{analysis_type}_PCA_plot.png"))
+  #show(ggplot(PCs_anc, aes(x=PC1,y=PC2))+
+  #  geom_point(aes(color=ancestry))+
+  #  colScale+
+  #  guides(color=guide_legend(title="Superpopulation"))+
+  #  ggtitle(glue("{id} {contrib_status} {analysis_type};\n{ncol(betaRedNAOmit)} ancestry SNPs"))+
+  #  theme(plot.title = element_text(hjust = 0.5)))
+  #dev.off()
+
+
+    #PCs_anc=merge(ancestry, PCs,by.x="id", by.y="V1")
+    ##PCs_anc$Size=ifelse(PCs_anc$reg=="Unk",10,9)
+
+    pal = unique(ancestry$superpop_color)
+    pal = setNames(pal, unique(ancestry$reg))
+
+
+    fig = plot_ly(PCs_anc, x = ~PC1, y = ~PC2, z = ~PC3, color = ~reg, colors=pal, size=10)
+    fig = fig %>% add_markers()
+    fig = fig %>% layout(scene = list(xaxis = list(title = 'PC1'),
+                                       yaxis = list(title = 'PC2'),
+                                       zaxis = list(title = 'PC3')))
+    htmlwidgets::saveWidget(as_widget(fig), glue("{path}/{id}_{contrib_status}_{analysis_type}_superpop_3D_PCAPlot.html"))
+    fig_sub = plot_ly(PCs_anc, x = ~PC1, y = ~PC2, z = ~PC3, color = ~population, colors=pal, size=10)
+    fig_sub = fig_sub %>% add_markers()
+    fig_sub = fig_sub %>% layout(scene = list(xaxis = list(title = 'PC1'),
+                                       yaxis = list(title = 'PC2'),
+                                       zaxis = list(title = 'PC3')))
+    htmlwidgets::saveWidget(as_widget(fig), glue("{path}/{id}_{contrib_status}_{analysis_type}_subpopulations_3D_PCAPlot.html"))
 }
