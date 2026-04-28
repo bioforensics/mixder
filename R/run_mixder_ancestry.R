95dae12644cf64e5a306eb3eede0376761b45d92
rnmitchell <rebecca.mitchell5987@gmail.com>
2026-04-28 08:18:17 -0400

    update docs again

--- R/run_mixder_ancestry.R
+++ R/run_mixder_ancestry.R
@@ -10,15 +10,15 @@
 # -------------------------------------------------------------------------------------------------
 #' run MixDeR using the CLI; ancestry prediction
 #'
-#' @param sample_reports directory of sample reports or CSV file of mixture genotypes (default=current directory)
-#' @param output output directory (default=current directory)
-#' @param sample_manifest path to sample manifest (default=NULL)
+#' @param sample_manifest path to sample manifest file (default=NULL)
+#' @param sample_reports Path to directory of Sample Reports or CSV file of mixture genotypes (default=current directory)
+#' @param output name of output directory, will be outputted into the sample reports directory (default="output")
 #' @param twofreqs TRUE if using different allele frequency data for each contributor (default=FALSE)
-#' @param freq_both frequency data for both contributors (default=1000G global)
-#' @param freq_major frequency data for major contributor (default=NULL)
-#' @param freq_minor frequeny data for minor contributor (default=NULL)
+#' @param freq_both frequency data for both contributors; requires twofreqs=FALSE (default=1000G global)
+#' @param freq_major frequency data for major contributor; requires twofreqs=TRUE (default=NULL)
+#' @param freq_minor frequency data for minor contributor; requires twofreqs=TRUE (default=NULL)
 #' @param refpath Path to directory containing reference genotypes (default=NULL)
-#' @param refs list of sample IDs to use as reference for conditioning, can specify more than one (default=NULL, example: ("Ref1", "Ref2"))
+#' @param refs list of sample IDs to use as reference for conditioning, can specify more than one (default=NULL, example: list("Ref1", "Ref2"))
 #' @param mixdeconv whether to run mixture deconvolution (default=TRUE)
 #' @param uncond run (or use) an unconditioned mixture deconvolution (default=TRUE)
 #' @param cond run (or use) a conditioned mixture deconvolution (default=FALSE)
@@ -36,7 +36,7 @@
 #'
 #' @export
 #'
-run_mixder_ancestry = function(sample_manifest, sample_reports = ".", output = ".", refpath=NULL, refs=NULL, mixdeconv=TRUE, uncond=TRUE, cond=FALSE, sets=10, dynamicAT=0.015, staticAT=10, minimum_snps=6000, A1_threshold=0.99, A2_threshold=0.6, minor_contrib_threshold=FALSE, keep_bins=TRUE, snps, pcagroups) {
+run_mixder_ancestry = function(sample_manifest, sample_reports = ".", output = "output", refpath=NULL, refs=NULL, mixdeconv=TRUE, uncond=TRUE, cond=FALSE, sets=10, dynamicAT=0.015, staticAT=10, minimum_snps=6000, A1_threshold=0.99, A2_threshold=0.6, minor_contrib_threshold=FALSE, keep_bins=TRUE, snps, pcagroups) {
   ## load in references
   if (isTruthy(refpath)) {
     print("loading references")
--- R/run_mixder_report.R
+++ R/run_mixder_report.R
@@ -10,15 +10,15 @@
 # -------------------------------------------------------------------------------------------------
 #' run MixDeR using the CLI; create GEDmatch PRO report(s)
 #'
-#' @param sample_reports directory of sample reports or CSV file of mixture genotypes (default=current directory)
-#' @param output output directory (default=current directory)
-#' @param sample_manifest path to sample manifest (default=NULL)
+#' @param sample_manifest path to sample manifest file (default=NULL)
+#' @param sample_reports Path to directory of Sample Reports or CSV file of mixture genotypes (default=current directory)
+#' @param output name of output directory, will be outputted into the sample reports directory (default="output")
 #' @param twofreqs TRUE if using different allele frequency data for each contributor (default=FALSE)
-#' @param freq_both frequency data for both contributors (default=1000G global)
-#' @param freq_major frequency data for major contributor (default=NULL)
-#' @param freq_minor frequeny data for minor contributor (default=NULL)
+#' @param freq_both frequency data for both contributors; requires twofreqs=FALSE (default=1000G global)
+#' @param freq_major frequency data for major contributor; requires twofreqs=TRUE (default=NULL)
+#' @param freq_minor frequency data for minor contributor; requires twofreqs=TRUE (default=NULL)
 #' @param refpath Path to directory containing reference genotypes (default=NULL)
-#' @param refs list of sample IDs to use as reference for conditioning, can specify more than one (default=NULL, example: ("Ref1", "Ref2"))
+#' @param refs list of sample IDs to use as reference for conditioning, can specify more than one (default=NULL, example: list("Ref1", "Ref2"))
 #' @param mixdeconv whether to run mixture deconvolution (default=TRUE)
 #' @param uncond run (or use) an unconditioned mixture deconvolution (default=TRUE)
 #' @param cond run (or use) a conditioned mixture deconvolution (default=FALSE)
@@ -34,7 +34,7 @@
 #'
 #' @export
 #'
-run_mixder_report = function(sample_manifest, sample_reports = ".", output = ".", twofreqs=FALSE, freq_both="global_1000g", freq_major=NULL, freq_minor=NULL, refpath=NULL, refs=NULL, mixdeconv=TRUE, uncond=TRUE, cond=FALSE, sets=10, dynamicAT=0.015, staticAT=10, minimum_snps=6000, A1_threshold=0.99, A2_threshold=0.6, filter_missing=FALSE, minor_contrib_threshold=FALSE, keep_bins=TRUE) {
+run_mixder_report = function(sample_manifest, sample_reports = ".", output = "output", twofreqs=FALSE, freq_both="global_1000g", freq_major=NULL, freq_minor=NULL, refpath=NULL, refs=NULL, mixdeconv=TRUE, uncond=TRUE, cond=FALSE, sets=10, dynamicAT=0.015, staticAT=10, minimum_snps=6000, A1_threshold=0.99, A2_threshold=0.6, filter_missing=FALSE, minor_contrib_threshold=FALSE, keep_bins=TRUE) {
   ## load in references
   if (isTruthy(refpath)) {
     print("loading references")
