e9bbc83e5ba0d6c43ecaa04bbb082366519be68c
rnmitchell <rebecca.mitchell5987@gmail.com>
2026-05-06 12:33:51 -0400

    updating tests

--- R/run_indiv_efm_set.R
+++ R/run_indiv_efm_set.R
@@ -36,7 +36,6 @@ run_indiv_efm_set = function(i, ids, snps_input, popFreq, refData, id, replicate
     replicate = replicate_id
   }
   if (!file.exists(glue("{write_path}/rda_files/{sample}.rda"))) {
-    print("creating evid")
     evidData = create_evid(sample, replicate, snps_input)
     dir.create(file.path(write_path, "rda_files"), showWarnings = FALSE, recursive=TRUE)
     save(evidData, file=glue("{write_path}/rda_files/{sample}.rda"))
@@ -62,13 +61,11 @@ run_indiv_efm_set = function(i, ids, snps_input, popFreq, refData, id, replicate
       repeat {
         message(glue("Running unconditioned analysis for set {i}, attempt #{repeat_num+1}<br/>"))
         if (substr(efm_v, 1,3)!="4.0" & substr(efm_v, 1,2) != "3.") {
-          uncond_results = euroformix::calcMLE(2, evidData, popFreq, AT=sample_at, BWS=FALSE, FWS=FALSE, DEG=FALSE, steptol=0.001, pC=0.01, lambda=0.05, fst=0.01, resttol=0, verbose=TRUE)
+          uncond_results = euroformix::calcMLE(2, evidData, popFreq, AT=sample_at, BWS=FALSE, FWS=FALSE, DEG=FALSE, steptol=0.001, pC=0.01, lambda=0.05, fst=0.01, resttol=0)
         } else {
-          uncond_results = euroformix::calcMLE(2, evidData, popFreq, AT=sample_at, BWS=FALSE, FWS=FALSE, DEG=FALSE, steptol=0.001, pC=0.01, lambda=0.05, fst=0.01,  verbose=TRUE)
+          uncond_results = euroformix::calcMLE(2, evidData, popFreq, AT=sample_at, BWS=FALSE, FWS=FALSE, DEG=FALSE, steptol=0.001, pC=0.01, lambda=0.05, fst=0.01)
         }
-        print('r')
         uncond_finaltable = euroformix::deconvolve(uncond_results)
-        print("1")
         if (check_allele_probabilities(data.frame(uncond_finaltable[["table4"]]), i)) break
         message(glue("Set {i} unconditioned: Mixture proportion = 0.5 or Allele probability flipping detected - will rerun!<br/>"))
         repeat_num = repeat_num + 1
--- tests/testthat/test-checking_af.R
+++ tests/testthat/test-checking_af.R
@@ -1,6 +1,7 @@
 test_that("Check AF file format and format if necessary", {
-  test_af = test_path("testdata", "freq_EAS.csv")
-  expect_equal(length(checking_af(test_af)), 10039)
-  test_af_notformatted = test_path("testdata", "AF_notformatted.csv")
-  expect_equal(length(checking_af(test_af_notformatted)), 5)
+  tmpdir = tempdir()
+  file.copy(test_path("testdata", "freq_EAS.csv"), tmpdir)
+  expect_equal(length(checking_af(paste0(tmpdir, "/freq_EAS.csv"))), 10039)
+  file.copy(test_path("testdata", "AF_notformatted.csv"), tmpdir)
+  expect_equal(length(checking_af(paste0(tmpdir, "/AF_notformatted.csv"))), 5)
 })
