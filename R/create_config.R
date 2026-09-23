a19d220e49ef502caae41fc91855fa0995a18dcf
rnmitchell <rebecca.mitchell5987@gmail.com>
2026-09-23 15:18:25 -0400

    fix config bug

--- R/create_config.R
+++ R/create_config.R
@@ -51,7 +51,7 @@ create_config = function(date, twofreqs, freq_all, freq_major, freq_minor, refs,
   config = setNames(data.frame(matrix(ncol=2, nrow=0)), c("Setting", "Value"))
   config = rbind(config, data.frame(Setting="MixDeR Version:", Value=getNamespaceVersion("mixder")[["version"]]))
   config = rbind(config, data.frame(Setting="EuroForMix Version:", Value=getNamespaceVersion("euroformix")[["version"]]))
-  onfig = rbind(config, data.frame(Setting="Assay:", Value=assay))
+  config = rbind(config, data.frame(Setting="Assay:", Value=assay))
   if (assay == "custom" & method == "Create GEDmatch PRO Report") {
     config = rbind(config, data.frame(Setting="SNP hg19 positions file for custom assay:", Value=pos))
   }
